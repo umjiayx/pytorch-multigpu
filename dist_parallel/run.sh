32326f789c18b215a32432cd04340a79d28f02c5
umjiayx <jiayx@umich.edu>
2024-10-04 03:39:12 -0400

    Minor Update

--- dist_parallel/run.sh
+++ dist_parallel/run.sh
@@ -11,16 +11,4 @@
 #SBATCH --output=output_%j.log       # Output log file
 #SBATCH --error=error_%j.log         # Error log file
 
-# Load necessary modules
-module load cuda/11.7
-module load python/3.9
-
-# Print the environment details to debug
-echo "Conda environment path: /scratch/qingqu_root/qingqu/jiayx/test1"
-echo "Activating environment..."
-
-# Directly run Python from the Conda environment
-/scratch/qingqu_root/qingqu/jiayx/test1/bin/python -c "import torch; print(torch.__version__)"
-
-# Run the Python script with PyTorch from the same Python interpreter
 /scratch/qingqu_root/qingqu/jiayx/test1/bin/python train.py --gpu_device 0 1 2 3 --batch_size 768
